5ff0704f854ba2a52d2532cb2efa078bb4bdde28
mgunnar <devmatheus@gmail.com>
2022-01-10 11:52:22 -0300

    removendo deste commit src/app/entities/categories.ts

--- src/app/entities/categories.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from 'mongoose';
-import { Guides } from '@entities/guides';
-
-export interface Categories {
-  title: string;
-  shortDescription: string;
-  guide: Guides;
-  parentCategory: Categories;
-}
